82f303f8f2649f52d6f168cce5f18aa5ff105e99
Patrick Harl <therandomwoodworker@gmail.com>
2020-04-14 18:46:46 -0700

    completed project and stretch

--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -5,21 +5,19 @@ import Post from "./Post";
 import "./Posts.css";
 
 // import data 
-import dummyData from 'C:/Users/thera/Desktop/LambdaAssignments/week 6/React-Components-Insta-Clone/src/dummy-data.js'
 
-const PostsPage = () => {
+
+const PostsPage = (props) => {
   // set up state for your data
-  const [posts, setPosts] = useState(dummyData)
-  
-  
-  
+  const [posts, setPosts] = useState(props.filteredData)
+   
 
   return (
     <div className="posts-container-wrapper">
       {/* map through data here to return a Post and pass data as props to Post */}
       {
 
-      posts.map(post => {
+      props.filteredData.map(post => {
 
         return <Post post={post} />
 
